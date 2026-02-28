9dba4153845411470334b709e2b20b79c5d2fe97
ejm5854 <ejm5854@gmail.com>
2026-02-28 15:30:41 -0800

    Stub out legacy jsx files to prevent conflicts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,2 @@
-import Hero from '../components/Hero'
-import TripCards from '../components/TripCards'
-import Stats from '../components/Stats'
-import Polaroids from '../components/Polaroids'
-
-export default function Home() {
-  return (
-    <main>
-      <Hero />
-      <Stats />
-      <TripCards />
-      <Polaroids />
-    </main>
-  )
-}
+// Home handled by Home.tsx
+export {};
